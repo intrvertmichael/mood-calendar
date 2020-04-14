92378b8cc59c9ee74d889288a63bb85f446aa039
intrvertmichael <michaelpaguay@gmail.com>
2020-04-14 11:11:15 -0400

    seems npm gh-pages built the react app into site

---
+++ build/precache-manifest.385b33a1768271d55cbd7e2e371f62cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9779ebfcee305270db32af6d4018da69",
+    "url": "/mood-calendar/index.html"
+  },
+  {
+    "revision": "a6e435b1cc8c629948ce",
+    "url": "/mood-calendar/static/css/main.732af123.chunk.css"
+  },
+  {
+    "revision": "c53942c2debb60c15188",
+    "url": "/mood-calendar/static/js/2.f482d693.chunk.js"
+  },
+  {
+    "revision": "86945b468f838a43d2e8e37fe67146ba",
+    "url": "/mood-calendar/static/js/2.f482d693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e435b1cc8c629948ce",
+    "url": "/mood-calendar/static/js/main.f861701e.chunk.js"
+  },
+  {
+    "revision": "2504460cfe896a769bde",
+    "url": "/mood-calendar/static/js/runtime-main.7dc09530.js"
+  }
+]);
