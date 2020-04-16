36917cca2148904037c801e2d8d5e4b86d8f7359
intrvertmichael <michaelpaguay@gmail.com>
2020-04-16 13:59:35 -0400

    Updates

--- precache-manifest.5ae90cb96966080087ccbbe93a7f59e4.js
+++ precache-manifest.5ae90cb96966080087ccbbe93a7f59e4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a726fb09e54599ad46a26479a161b684",
+    "revision": "89762aecd78c96e43ea37cdfa3ab22d3",
     "url": "/mood-calendar/index.html"
   },
   {
-    "revision": "bc84cb372560487053b1",
-    "url": "/mood-calendar/static/css/main.41ab1907.chunk.css"
+    "revision": "7a837570c69409731665",
+    "url": "/mood-calendar/static/css/main.3dd6ab35.chunk.css"
   },
   {
     "revision": "c53942c2debb60c15188",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mood-calendar/static/js/2.f482d693.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bc84cb372560487053b1",
+    "revision": "7a837570c69409731665",
     "url": "/mood-calendar/static/js/main.901bfc06.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mood-calendar/precache-manifest.f176789961a1f16936f30d696bbba046.js"
+  "/mood-calendar/precache-manifest.5ae90cb96966080087ccbbe93a7f59e4.js"
 );
 
 self.addEventListener('message', (event) => {
